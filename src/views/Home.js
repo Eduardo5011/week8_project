15f49905b81150fd03274cb37d9c64633769661d
Eduardo5011 <eduardo.rosado5011@gmail.com>
2021-12-01 18:00:08 -0500

    update

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                Hello
+                Oh hi there, welcome to el paraiso Fakeshop... <a href="/shop">click here</a>
             </div>
         )
     }
